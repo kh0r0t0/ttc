602a28893a6f37c9fedf46f6f7983ccf1f5c7a40
kh0r0t0 <elmadisamir2002@gmail.com>
2024-11-30 17:36:41 +0000

    ***.

---
+++ main.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+
+
+int main(void){
+	// code here 
+	// calculator 
+	//
+	//
+	//
+	
+	int datedenaiss= 2024;
+	
+	int birthday_year;
+	
+	int age = datedenaiss - birthday_year;
+
+	printf(" || HOW OLD I'AM I ? || \n ");
+	
+	printf("Enter your birthday year :   ");
+	scanf(" %d ", &birthday_year );
+	
+	printf(" This is your birthday : %d \n ", birthday_year );
+
+	printf(" YOUR AGE IS : %d \n ", age );
+
+	printf(" HAVE A NICE DAY! \n ");
+
+		
+
+
+	return 0;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
