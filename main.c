f8528f87e21aa92f66e00dc5bf257a2134d777d6
kh0r0t0 <elmadisamir2002@gmail.com>
2024-12-08 22:57:08 +0000

    Update main.c

--- main.c
+++ main.c
@@ -3,32 +3,12 @@
 
 
 int main(void){
-	// code here 
-	// calculator 
-	//
-	//
-	//
+	// a buffer 
+	int long string_f = 1000;
+	float pi = 3.14;
+
+	printf(" Hello, World! ");
 	
-	int datedenaiss= 2024;
-	
-	int birthday_year;
-	
-	int age = datedenaiss - birthday_year;
-
-	printf(" || HOW OLD I'AM I ? || \n ");
-	
-	printf("Enter your birthday year :   ");
-	scanf(" %d ", &birthday_year );
-	
-	printf(" This is your birthday : %d \n ", birthday_year );
-
-	printf(" YOUR AGE IS : %d \n ", age );
-
-	printf(" HAVE A NICE DAY! \n ");
-
-		
-
-
 	return 0;
 
 
