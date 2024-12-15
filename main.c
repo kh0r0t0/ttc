aebb7c87a36207e4d31e9b521a075385df12cbbe
kh0r0t0 <elmadisamir2002@gmail.com>
2024-12-15 16:06:44 +0000

    --rename

---
+++ main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.c>
+
+
+
+int main(void){
+	unsigned int vone = 1000;
+	unsigned int vtwo = 2000;
+	
+
+	printf("  Ttc!!!  \n ");
+	return 0;
+}
+
+
+
+
+
+
